2596aa6e8237a378bb4dcd4d127b705205494466
Rio Edwards <rioredwards@gmail.com>
2022-11-16 13:12:33 -0800

    Add Route for error/teapot

--- __tests__/error-handling.test.js
+++ __tests__/error-handling.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('error handling', () => {
-  it.skip('/error/teapot should return a 418', async () => {
+  it('/error/teapot should return a 418', async () => {
     const resp = await request(app).get('/error/teapot');
     expect(resp.status).toBe(418);
   });
